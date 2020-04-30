4a44ac70ef08bc3b887f103ad386ffa9b5197976
Meghan Hein <meghan.hein@gmail.com>
2020-04-30 18:07:40 +0000

    Initial commit with javascript prework

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function math (waste, of, time) {
+  return of * time + waste;
+}
+console.log(math(53, 61, 67));
---
+++ rounding-numbers.js
@@ -0,0 +1,4 @@
+const roundUp = 1.5;
+Math.round(roundUp);
+const rounded = Math.round(roundUp);
+console.log(rounded);
